1f5c630ea7ec07888f128dfa91d3f6663db6b628
Dan Palmer <boss@bitrecursive.com>
2016-06-11 01:41:51 +0100

    Search for an Escort by Detainee prison number
    We need a way to lookup escorts from the search page
    based on prison number.
    
    This is something that can be used in the form to return
    results/no results if the prison number is valid.

--- app/models/escort.rb
+++ app/models/escort.rb
@@ -1,3 +1,7 @@
 class Escort < ApplicationRecord
   has_one :detainee, dependent: :destroy
+
+  def self.find_detainee_by_prison_number(number)
+    joins(:detainee).where(detainees: { prison_number: number }).first
+  end
 end
