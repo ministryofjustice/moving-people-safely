f940fc2a1217437dbc2bc0e89907033167e80a84
Dan Palmer <boss@bitrecursive.com>
2016-06-11 19:53:19 +0100

    Make cell rendering available to AppCtrller
    This is common functionality that will be required across
    most controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def render_cell(*cell_attrs)
+    render html: cell(*cell_attrs), layout: true
+  end
 end
