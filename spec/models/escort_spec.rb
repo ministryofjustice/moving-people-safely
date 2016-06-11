5801ee004e98fe493a28b903fa6b9127fbe3e709
Dan Palmer <boss@bitrecursive.com>
2016-06-11 01:41:51 +0100

    Search for an Escort by Detainee prison number
    We need a way to lookup escorts from the search page
    based on prison number.
    
    This is something that can be used in the form to return
    results/no results if the prison number is valid.

--- spec/models/escort_spec.rb
+++ spec/models/escort_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Escort, type: :model do
   it { is_expected.to have_one(:detainee).dependent(:destroy) }
+
+  describe '.find_detainee_by_prison_number' do
+    subject { described_class.find_detainee_by_prison_number('A1234BC') }
+
+    context 'when there is an associated matching detainee' do
+      it 'returns the escort' do
+        escort = Escort.create.tap do |e|
+          e.create_detainee(prison_number: 'A1234BC')
+        end
+
+        expect(subject).to eq escort
+      end
+    end
+
+    context 'when there is no associated matching detainee' do
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+  end
 end
