15ea3305157e9f214c54f12e53818ce900c68247
Dan Palmer <boss@bitrecursive.com>
2016-06-14 17:33:25 +0100

    Introduce ability to navigate healthcare forms

--- app/cells/healthcare_cell.rb
+++ app/cells/healthcare_cell.rb
@@ -1,7 +1,34 @@
+require 'cell/translation'
+
 class HealthcareCell < Cell::ViewModel
-  alias_method :form, :model
+  include ActionView::Helpers::TranslationHelper
+  include Cell::Translation
+
+  property :form
+
+  def title
+    t(".#{model.name}")
+  end
+
+  def back_link
+    link_to '< Back', model.prev_path
+  end
+
+  def next_link
+    link_to 'Skip >', model.next_path
+  end
+
+  def current_question_text
+    current_question = model.node_position
+    total_questions = Forms::Healthcare::StepManager.total_steps
+    "Question #{current_question} of #{total_questions}"
+  end
+
+  def template_name
+    model.name
+  end
 
   def form_path
-    physical_path(form.model.escort)
+    model.path
   end
 end
---
+++ app/models/forms/healthcare/step_manager.rb
@@ -0,0 +1,69 @@
+module Forms
+  module Healthcare
+    class StepManager
+      HEALTHCARE_STEPS = DoubleLinkedList.new.tap do |l|
+        l << Forms::Healthcare::Physical
+        l << Forms::Healthcare::Mental
+        l << Forms::Healthcare::Social
+      end
+
+      def self.build_step_for(step_name, escort)
+        node = HEALTHCARE_STEPS.find { |n| n.value.name == step_name }
+        form = node.value.new(escort.healthcare)
+        Step.new(node, form, escort)
+      end
+
+      def self.total_steps
+        HEALTHCARE_STEPS.count
+      end
+    end
+
+    class Step
+      include Rails.application.routes.url_helpers
+
+      def initialize(node, form, escort)
+        @node = node
+        @form = form
+        @escort = escort
+      end
+
+      attr_reader :form
+
+      delegate :position, :next, :prev, to: :@node, prefix: 'node'
+
+      def next_path
+        build_path_for_step(node_next)
+      end
+
+      def prev_path
+        build_path_for_step(node_prev)
+      end
+
+      def path
+        build_path_for_step(@node)
+      end
+
+      def has_next?
+        node_next.present?
+      end
+
+      def has_prev?
+        node_prev.present?
+      end
+
+      def name
+        @node.value.name
+      end
+
+      private
+
+      def build_path_for_step(step)
+        if step
+          public_send("#{step.value.name}_path", @escort)
+        else
+          profile_path(@escort)
+        end
+      end
+    end
+  end
+end
