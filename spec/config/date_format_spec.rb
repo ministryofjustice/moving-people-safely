c67b8b6f1e7fe12cd431690660be1147cd3ec464
Dan Palmer <boss@bitrecursive.com>
2016-06-21 22:20:48 +0100

    Use a humanized date format (DD Month YYYY)
    User research feedback suggested the dd/mm/yyyy format
    can be slower to interpret.

--- spec/config/date_format_spec.rb
+++ spec/config/date_format_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'custom date formats', type: :config do
       expect(example_date.to_s).to eq '10/03/1940'
     end
   end
+
+  describe 'humanized date format' do
+    it 'returns a date in the format DD/MM/YYYY' do
+      example_date = Date.civil(1940, 3, 10)
+      expect(example_date.to_s(:humanized)).to eq '10 Mar 1940'
+    end
+  end
 end
