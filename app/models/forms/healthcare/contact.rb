0127b50205ace473e5451313a401a1589e8d94bd
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-06-24 16:21:30 +0100

    Rename clinician name to healthcare professional

--- app/models/forms/healthcare/contact.rb
+++ app/models/forms/healthcare/contact.rb
@@ -1,7 +1,7 @@
 module Forms
   module Healthcare
     class Contact < Forms::Base
-      property :clinician_name, type: StrictString
+      property :healthcare_professional, type: StrictString
       property :contact_number, type: StrictString
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620145048) do
+ActiveRecord::Schema.define(version: 20160624151208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20160620145048) do
     t.string   "medication",               default: "unknown"
     t.string   "mpv",                      default: "unknown"
     t.text     "mpv_details"
-    t.string   "clinician_name"
+    t.string   "healthcare_professional"
     t.string   "contact_number"
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
