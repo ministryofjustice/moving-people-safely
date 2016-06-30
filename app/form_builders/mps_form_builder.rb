3c667a3adb89a10136f5ab93a8519f215a0ee7b7
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-06-30 10:05:20 +0100

    Improve MPS form builder
    This commit will allow to have radio toggles with a
    texture without a label

--- app/form_builders/mps_form_builder.rb
+++ app/form_builders/mps_form_builder.rb
@@ -1,18 +1,37 @@
 class MpsFormBuilder < GovukElementsFormBuilder::FormBuilder
-  def radio_toggle(attribute, &_blk)
+  # rubocop:disable MethodLength
+  def radio_toggle(attribute, attribute_with_error = nil, &_blk)
+    style = 'optional-section-wrapper'
+    style << ' panel panel-border-narrow' unless error_for? attribute_with_error
     content_tag(:div, class: 'js-show-hide') do
       safe_join([
         content_tag(:div, class: 'form-group controls-optional-section') do
           radio_button_fieldset attribute,
             choices: object.toggle_choices, inline: true
         end,
-        content_tag(:div, class: 'optional-section-wrapper') { yield }
+        content_tag(:div, class: style) { yield }
       ])
     end
   end
 
   def radio_toggle_with_textarea(attribute)
-    radio_toggle(attribute) { text_area :"#{attribute}_details" }
+    attribute_details = :"#{attribute}_details"
+    radio_toggle(attribute, attribute_details) do
+      text_area_without_label attribute_details
+    end
+  end
+
+  def text_area_without_label(attribute)
+    content_tag :div,
+      class: form_group_classes(attribute),
+      id: form_group_id(attribute) do
+      text_area_tag =
+        ActionView::Helpers::Tags::TextArea.new(
+          object.class.name, attribute, self, class: 'form-control'
+        ).render
+      hint_tag = content_tag(:span, hint_text(attribute), class: 'form-hint')
+      (hint_tag + text_area_tag).html_safe
+    end
   end
 
   def label_with_radio(attribute, text, value)
