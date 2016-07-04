42bfe2203098dc399dab63f47b65e493f39a2ab3
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-04 18:19:31 +0100

    TextDatevalidator

--- spec/forms/offences_form_spec.rb
+++ spec/forms/offences_form_spec.rb
@@ -17,40 +17,7 @@ RSpec.describe OffencesForm, type: :form do
     let(:result) { subject.validate(form_data) }
 
     it { is_expected.to validate_presence_of(:release_date) }
-
-    context "release_date" do
-      context "with a text date in UK format (DD/MM/YYYY)" do
-        let(:release_date) { '31/01/2017' }
-
-        it "is valid" do
-          expect(result).to be true
-        end
-      end
-
-      context "with text that cannot be coerced to a date" do
-        let(:release_date) { 'this is no format for a date' }
-
-        it "is invalid" do
-          expect(result).to be false
-        end
-
-        it "sets an error message" do
-          expect(subject.errors[:release_date]).to include "sdf"
-        end
-      end
-
-      context "with missing data" do
-        let(:release_date) { nil }
-
-        it "is invalid" do
-          expect(result).to be false
-        end
-
-        it "sets an error message" do
-          expect(subject.errors[:release_date]).to include "cannot be blank"
-        end
-      end
-    end
+    it { is_expected.to validate_string_as_date(:release_date) }
 
     context "not_for_release" do
       context "accepts boolean" do
