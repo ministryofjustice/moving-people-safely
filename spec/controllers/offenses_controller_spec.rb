e8a1e16ca57ce19c6ef111038903f5590917446b
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-04 12:12:14 +0100

    wip

--- spec/controllers/offenses_controller_spec.rb
+++ spec/controllers/offenses_controller_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe OffensesController, type: :controller do
   let(:escort) { Escort.create }
+  let(:user) { User.build }
 
   describe "#show" do
-    before { process :show, method: :get, params: { escort_id: escort.id } }
 
     context "not logged in" do
       it "redirects to /sign_in" do
+        process :show, method: :get, params: { escort_id: escort.id }
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     it "returns status 200" do
+      process :show, method: :get, params: { escort_id: escort.id }
       expect(response.status).to_be :ok
     end
 
