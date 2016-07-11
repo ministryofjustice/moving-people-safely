4d27098a9e809d24e82ee961a5d0f16314488578
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-11 09:34:08 +0100

    Method ordering

--- spec/support/matchers/validate_prepopulated_collection.rb
+++ spec/support/matchers/validate_prepopulated_collection.rb
@@ -27,88 +27,6 @@ class ValidatePrepopulatedCollection
 
   private
 
-  # if the has_* method was defined at run-time, our form has different behaviour
-  def has_defined_at_runtime?
-    subject.public_send("has_#{field_name}") == 'yes'
-  end
-
-  def params
-    {
-      "#{field_name}" => [subform_params, subform_params],
-      "has_#{field_name}" => 'yes'
-    }
-  end
-
-  def reset_subject
-    subject.public_send("#{field_name}=", [])
-    true
-  end
-
-  def handle_collection_deletion
-    unless has_defined_at_runtime?
-      p2 = params
-      p2["has_#{field_name}"] = 'no'
-      subject.validate(p2)
-      unless collection == []
-        set_error "Error handling form validation when nested collection should be empty. Received #{collection}"
-        return false
-      end
-    end
-    reset_subject
-  end
-
-  def handle_subform_deletion
-    p2 = params
-    p2[field_name.to_s][0]['_delete'] = '1'
-    subject.validate(p2)
-    unless collection.size == 1
-      set_error "Error handling nested model deletion. Received #{collection}."
-      return false
-    end
-    reset_subject
-  end
-
-  def validate_collection
-    subject.validate(params)
-    unless collection.size == 2
-      set_error "Failed to validate multiple nested #{field_name}. Got #{collection}"
-      return false
-    end
-    reset_subject
-  end
-
-  def subform_class
-    namespace = subject.class.to_s.deconstantize
-    const_name = [namespace, field_name.to_s.classify].join('::')
-    const_name.constantize
-  end
-
-  def subform_model_class
-    field_name.to_s.classify.constantize
-  end
-
-  def subform_params
-    params = {}
-    f = subform_class.new(subform_model_class.new)
-    f.schema.each do |sch|
-      options_hash = sch.instance_variable_get(:@options)
-      name = options_hash[:name]
-      type = options_hash[:type]
-
-      case
-      when name == 'carrier'
-        params[name] = 'detainee'
-      when type.to_s == "Forms::StrictString"
-        params[name] = 'xxxxx'
-      when name == '_delete'
-        params[name] = '0'
-      else
-        raise "Undefined subform property type!"
-      end
-    end
-    params
-  end
-
   def has_
     result = subject.respond_to?("has_#{field_name}".to_sym)
     unless result == true
@@ -136,6 +54,92 @@ class ValidatePrepopulatedCollection
       return false
     end
     reset_subject
+  end
+
+  def validate_collection
+    subject.validate(params)
+    unless collection.size == 2
+      set_error "Failed to validate multiple nested #{field_name}. Got #{collection}"
+      return false
+    end
+    reset_subject
+  end
+
+  def handle_subform_deletion
+    p2 = params
+    p2[field_name.to_s][0]['_delete'] = '1'
+    subject.validate(p2)
+    binding.pry
+    unless collection.size == 1
+      set_error "Error handling nested model deletion. Received #{collection}."
+      return false
+    end
+    reset_subject
+  end
+
+  def handle_collection_deletion
+    unless has_defined_at_runtime?
+      p2 = params
+      p2["has_#{field_name}"] = 'no'
+      subject.validate(p2)
+      unless collection == []
+        set_error "Error handling form validation when nested collection should be empty. Received #{collection}"
+        return false
+      end
+    end
+    reset_subject
+  end
+
+  # if the has_* method was defined at run-time, our form has different behaviour
+  def has_defined_at_runtime?
+    subject.public_send("has_#{field_name}") == 'yes'
+  end
+
+  def params
+    {
+      "#{field_name}" => [subform_params, subform_params],
+      "has_#{field_name}" => 'yes'
+    }
+  end
+
+  def reset_subject
+    subject.public_send("#{field_name}=", [])
+    true
+  end
+
+  def subform_class
+    namespace = subject.class.to_s.deconstantize
+    const_name = [namespace, field_name.to_s.classify].join('::')
+    const_name.constantize
+  end
+
+  def subform_model_class
+    field_name.to_s.classify.constantize
+  end
+
+  # attempt to generate workable params for the collection model
+  def subform_params
+    params = {}
+    f = subform_class.new(subform_model_class.new)
+    f.schema.each do |sch|
+      options_hash = sch.instance_variable_get(:@options)
+      name = options_hash[:name]
+      type = options_hash[:type]
+
+      case
+      when name == 'carrier'
+        params[name] = 'detainee'
+      when name == 'must_return'
+        params[name] = 'must_not_return'
+      when type.to_s == "Forms::StrictString"
+        params[name] = 'xxx DATA xxx'
+      when name == '_delete'
+        params[name] = '0'
+      else
+        raise "Undefined subform property type!"
+      end
+    end
+    params
   end
 
   def set_error(msg)
