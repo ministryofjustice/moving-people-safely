fa29dcc6c421074d4b26158bdfca8b5ac21aa8c7
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-12 14:19:42 +0100

    Only render on GET (#68)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,11 +1,11 @@
 class HomepageController < ApplicationController
   def show
+    form.validate(prison_number: params[:search]) if params[:search]
     render :show, locals: { form: form, escort: form.escort }
   end
 
   def search
-    form.validate(params[:search])
-    render :show, locals: { form: form, escort: form.escort }
+    redirect_to root_path(search: params[:search]['prison_number'])
   end
 
   private
