a31eef9beddd87d42588db57e6be21a49bc6788f
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-13 16:19:07 +0100

    Dashboard date picker (#73)
    * working draft of date picker
    
    * which integrates with the searh function
    
    * which is styled
    
    * specs
    
    * skip CSRF checks on homepage
    
    * bah rubocop

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,16 +1,55 @@
 class HomepageController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
-    form.validate(prison_number: params[:search]) if params[:search]
-    render :show, locals: { form: form, result: form.escort }
+    search_form.validate(prison_number: params[:search]) if params[:search]
+    render :show, locals: locals
   end
 
   def search
-    redirect_to root_path(search: params[:search]['prison_number'])
+    redirect_to root_path redirect_params(params[:search]['prison_number'])
+  end
+
+  def date
+    case params[:commit]
+    when 'today'
+      date_picker.today
+    when '<'
+      date_picker.back
+    when '>'
+      date_picker.forward
+    when 'Go'
+      date_picker.date = params[:date_picker]
+    end
+
+    session[:date_in_view] = date_picker.to_s
+    redirect_to root_path redirect_params(params[:search])
   end
 
   private
 
-  def form
-    @_form ||= Forms::Search.new
+  def redirect_params(search_query)
+    if search_query.present?
+      { search: search_query }
+    else
+      {}
+    end
+  end
+
+  def locals
+    {
+      search_form: search_form,
+      result: search_form.escort,
+      date_picker: date_picker,
+      prison_number: params[:search]
+    }
+  end
+
+  def date_picker
+    @_date_picker ||= DatePicker.new(session[:date_in_view])
+  end
+
+  def search_form
+    @_search_form ||= Forms::Search.new
   end
 end
---
+++ spec/models/date_picker_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+
+RSpec.describe DatePicker do
+  subject { described_class.new(date) }
+
+  context "initialized with a valid date string" do
+    let(:date) { '15/09/2027' }
+
+    describe "#date" do
+      let(:result) { subject.date }
+
+      it "returns a date object" do
+        expect(result).to be_a Date
+      end
+
+      it "returns the correct date" do
+        expect(result).to eql Date.strptime(date, '%d/%m/%Y')
+      end
+    end
+
+    describe "#to_s" do
+      let(:result) { subject.to_s }
+
+      it "returns the initializing date as correctly formatted string" do
+        expect(result).to eq date
+      end
+    end
+
+    describe "#date=" do
+      before { subject.date = new_date }
+
+      context "with a valid date string" do
+        let(:new_date) { '01/01/2001' }
+
+        it "sets the date" do
+          expect(subject.date).to eql Date.new(2001, 1, 1)
+        end
+
+        it "sets the date string" do
+          expect(subject.to_s).to eql new_date
+        end
+      end
+
+      context "with an invalid date string" do
+        let(:new_date) { 'elephant' }
+
+        it "doesn't change the #date" do
+          expect(subject.date).to eql Date.new(2027, 9, 15)
+        end
+
+        it "changes the date returned by #to_s" do
+          expect(subject.to_s).to eql new_date
+        end
+      end
+    end
+
+    context "date modifiers" do
+      let(:result) { subject.date }
+
+      describe "#forward" do
+        before { subject.forward }
+
+        it "increments the date by one day" do
+          expect(result).to eql (Date.strptime(date, '%d/%m/%Y') + 1.day)
+        end
+      end
+
+      describe "#back" do
+        before { subject.back }
+
+        it "decrements the date by one day" do
+          expect(result).to eql (Date.strptime(date, '%d/%m/%Y') - 1.day)
+        end
+      end
+
+      describe "#today" do
+        before { subject.today }
+
+        it "sets the date to today's date" do
+          expect(result).to eql Date.today
+        end
+      end
+    end
+  end
+
+  context "initialized with an invalid date string" do
+    let(:date) { '01/13/2016' }
+
+    describe "#date" do
+      let(:result) { subject.date }
+
+      it "returns today's date" do
+        expect(result).to eql Date.today
+      end
+    end
+
+    describe "#to_s" do
+      let(:result) { subject.to_s }
+
+      it "returns the initializing string" do
+        expect(result).to eq date
+      end
+    end
+
+    describe "#forward" do
+      before { subject.forward }
+
+      it "#date returns tomorrows date" do
+        expect(subject.date).to eql Date.tomorrow
+      end
+
+      it "#to_s returns the initializing string" do
+        expect(subject.to_s).to eql date
+      end
+    end
+  end
+end
