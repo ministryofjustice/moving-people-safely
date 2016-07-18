7ff7df69d3c40ac10587bc1a8634542f07b38d81
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-18 17:45:13 +0100

    Escort.create_with_children

--- spec/factories/escort_factory.rb
+++ spec/factories/escort_factory.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :escort do
     association :detainee, factory: :detainee, strategy: :build
     association :move, factory: :move, strategy: :build
+    association :healthcare, factory: :healthcare, strategy: :build
+    association :risks, factory: :risks, strategy: :build
     association :offences, factory: :offences, strategy: :build
 
     trait :with_past_move do
@@ -10,6 +12,18 @@ FactoryGirl.define do
 
     trait :with_future_move do
       association :move, :future_move, factory: :move, strategy: :build
+    end
+
+    trait :with_incomplete_healthcare do
+      association :healthcare, :incomplete, factory: :healthcare, strategy: :build
+    end
+
+    trait :with_incomplete_risks do
+      association :risks, :incomplete, factory: :risks, strategy: :build
+    end
+
+    trait :with_incomplete_offences do
+      association :offences, :incomplete, factory: :offences, strategy: :build
     end
   end
 end
