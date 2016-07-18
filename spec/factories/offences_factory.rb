dbf7bde120cb3d98d58178c6fe2d90eb503c08de
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-18 18:03:53 +0100

    factories

--- spec/factories/offences_factory.rb
+++ spec/factories/offences_factory.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :offences do
     release_date '15/09/2027'
+    workflow_status 'complete'
+
+    trait :incomplete do
+      workflow_status 'incomplete'
+    end
   end
 end
