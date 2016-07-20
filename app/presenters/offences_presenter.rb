834e673bca35c5fd777b4bbd0070aa5e69184fa0
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-07-20 12:01:28 +0100

    Use slim to render html_safe strings (#91)

--- app/presenters/offences_presenter.rb
+++ app/presenters/offences_presenter.rb
@@ -1,9 +1,9 @@
 class OffencesPresenter < SimpleDelegator
   def styled_current_offences
-    current_offences.map(&:offence).join('<br>').html_safe
+    current_offences.map(&:offence).join('<br>')
   end
 
   def styled_past_offences
-    past_offences.map(&:offence).join('<br>').html_safe
+    past_offences.map(&:offence).join('<br>')
   end
 end
