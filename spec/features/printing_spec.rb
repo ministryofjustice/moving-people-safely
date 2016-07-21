60cd126081a63ba95eabce9b931ccb6d72c164b7
Tom Gladhill <whoojemaflip@gmail.com>
2016-07-21 15:12:30 +0100

    Feature for preliminary Print work (#100)
    * SSSSPECSSSS
    
    * fixup! SSSSPECSSSS
    
    * fixup! fixup! SSSSPECSSSS

---
+++ spec/features/printing_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'printing a PER', type: :feature do
+  scenario 'user prints a completed PER' do
+    escort = create :escort
+    prison_number = escort.detainee.prison_number
+
+    login
+    print_PER(prison_number)
+
+    within("tr#prison_number_#{prison_number}") do
+      assert has_no_link? "#{prison_number}"
+      assert has_content? 'Issued'
+      assert has_link? 'Reprint'
+    end
+  end
+
+  def print_PER(prison_number)
+    within("tr#prison_number_#{prison_number}") do
+      click_link 'Print'
+    end
+  end
+end
