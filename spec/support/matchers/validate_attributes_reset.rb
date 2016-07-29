86af76b258b14accdca9121d77180c97e7769cac
Dan Palmer <boss@bitrecursive.com>
2016-07-29 17:15:41 +0100

    Provide a matcher to spec resetting attributes

---
+++ spec/support/matchers/validate_attributes_reset.rb
@@ -0,0 +1,81 @@
+class ValidateAttributesReset
+  attr_reader :attributes, :toggle_attribute, :subject, :error
+
+  def initialize(*attributes)
+    @attributes = attributes
+  end
+
+  def matches?(subject)
+    @subject = subject
+
+    validate_attributes_retained_when_toggle_attribute_is_yes &&
+      validate_attributes_reset_when_toggle_attribute_is("no") &&
+      validate_attributes_reset_when_toggle_attribute_is("unknown")
+  end
+
+  def when_attribute_is_disabled(attribute)
+    @toggle_attribute = attribute
+    self
+  end
+
+  def description
+    "validate that #{attributes.to_sentence} are reset when #{toggle_attribute} is disabled."
+  end
+
+  def failure_message
+    "Expected #{attributes.to_sentence} to be reset when #{toggle_attribute} is disabled.\n#{error}"
+  end
+
+  private
+
+  MOCK_USER_INPUT = "some user input"
+
+  def validate_attributes_retained_when_toggle_attribute_is_yes
+    populate_attributes_under_test
+    set_toggle_attribute_to "yes"
+    simulate_form_being_validated
+
+    attributes.all? do |attr|
+      result = subject.public_send(attr) == MOCK_USER_INPUT
+
+      unless result
+        set_error "Expected #{attr} to be retained when #{toggle_attribute} set to yes"
+      end
+
+      result
+    end
+  end
+
+  def validate_attributes_reset_when_toggle_attribute_is(value)
+    populate_attributes_under_test
+    set_toggle_attribute_to value
+    simulate_form_being_validated
+
+    attributes.all? do |attr|
+      result = subject.public_send(attr).nil?
+
+      unless result
+        set_error "Expected #{attr} to be reset when #{toggle_attribute} set to #{value}"
+      end
+
+      result
+    end
+  end
+
+  def populate_attributes_under_test
+    attributes.each { |attr| subject.public_send("#{attr}=", MOCK_USER_INPUT) }
+  end
+
+  def set_toggle_attribute_to(value)
+    subject.public_send("#{toggle_attribute}=", value)
+  end
+
+  def simulate_form_being_validated
+    fake_web_params = {}
+    subject.validate(fake_web_params)
+  end
+
+  def set_error(msg)
+    @error = msg
+  end
+end
