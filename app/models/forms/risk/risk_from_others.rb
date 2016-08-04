93b1957571106ceca5383d42541cbca257133afc
Dan Palmer <boss@bitrecursive.com>
2016-08-04 13:46:11 +0100

    Isolate CSRA section in form using class method
    These two things are inextricably linked on the form,
    forms don't have the concept of nesting/associations etc.
    Figure this approach makes it a little easier at first
    glance for the reader to realise this is the case.

--- app/models/forms/risk/risk_from_others.rb
+++ app/models/forms/risk/risk_from_others.rb
@@ -5,19 +5,22 @@ module Forms
       CSRA_STANDARD = 'standard'
       CSRA_TOGGLE_CHOICES = [CSRA_HIGH, CSRA_STANDARD, DEFAULT_CHOICE]
 
+      def self.configure_csra_section
+        property(:csra, type: StrictString, default: DEFAULT_CHOICE)
+        validates :csra,
+          inclusion: { in: CSRA_TOGGLE_CHOICES },
+          allow_blank: true
+
+        property(:csra_details, type: StrictString)
+        validates :csra_details,
+          presence: true,
+          if: -> { csra == CSRA_HIGH || csra == CSRA_STANDARD }
+      end
+
       optional_details_field :rule_45
+      configure_csra_section
       optional_details_field :verbal_abuse
       optional_details_field :physical_abuse
-
-      property(:csra, type: StrictString, default: DEFAULT_CHOICE)
-      validates :csra,
-        inclusion: { in: CSRA_TOGGLE_CHOICES },
-        allow_blank: true
-
-      property(:csra_details, type: StrictString)
-      validates :csra_details,
-        presence: true,
-        if: -> { csra == CSRA_HIGH || csra == CSRA_STANDARD }
 
       def csra_toggle_choices
         CSRA_TOGGLE_CHOICES
