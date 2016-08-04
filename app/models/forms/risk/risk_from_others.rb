af8d222b67741ac72d3850bcaf895f4a7d4c33b3
Dan Palmer <boss@bitrecursive.com>
2016-08-04 17:55:06 +0100

    Bite-sized separation of concerns for CSRA section

--- app/models/forms/risk/risk_from_others.rb
+++ app/models/forms/risk/risk_from_others.rb
@@ -1,29 +1,30 @@
 module Forms
   module Risk
     class RiskFromOthers < Forms::Base
-      CSRA_HIGH = 'high'
-      CSRA_STANDARD = 'standard'
-      CSRA_TOGGLE_CHOICES = [CSRA_HIGH, CSRA_STANDARD, DEFAULT_CHOICE]
-
-      def self.configure_csra_section
-        property(:csra, type: StrictString, default: DEFAULT_CHOICE)
-        validates :csra,
-          inclusion: { in: CSRA_TOGGLE_CHOICES },
-          allow_blank: true
-
-        property(:csra_details, type: StrictString)
-        validates :csra_details,
-          presence: true,
-          if: -> { csra == CSRA_HIGH }
-      end
-
       optional_details_field :rule_45
-      configure_csra_section
       optional_details_field :verbal_abuse
       optional_details_field :physical_abuse
 
-      def csra_toggle_choices
-        CSRA_TOGGLE_CHOICES
+      concerning :CsraSection do
+        included do
+          CSRA_HIGH = 'high'
+          CSRA_STANDARD = 'standard'
+          CSRA_TOGGLE_CHOICES = [CSRA_HIGH, CSRA_STANDARD, DEFAULT_CHOICE]
+
+          property(:csra, type: StrictString, default: DEFAULT_CHOICE)
+          validates :csra,
+            inclusion: { in: CSRA_TOGGLE_CHOICES },
+            allow_blank: true
+
+          property(:csra_details, type: StrictString)
+          validates :csra_details,
+            presence: true,
+            if: -> { csra == CSRA_HIGH }
+        end
+
+        def csra_toggle_choices
+          CSRA_TOGGLE_CHOICES
+        end
       end
     end
   end
