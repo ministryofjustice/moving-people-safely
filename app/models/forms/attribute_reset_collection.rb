0a1c751febf77fd1d11a39b6b0486a26f3bedd4a
Dan Palmer <boss@bitrecursive.com>
2016-08-12 14:23:55 +0100

    Add a collection obj for resetting form attributes

---
+++ app/models/forms/attribute_reset_collection.rb
@@ -0,0 +1,23 @@
+module Forms
+  class AttributeResetCollection
+    ResetData = Struct.new(:attributes_to_reset, :master_attribute, :enabled_value)
+
+    def initialize
+      @collection = []
+    end
+
+    def add(*args)
+      @collection << ResetData.new(*args)
+    end
+
+    def perform(form)
+      @collection.each do |reset_obj|
+        if form.public_send(reset_obj.master_attribute) != reset_obj.enabled_value
+          reset_obj.attributes_to_reset.each do |attribute|
+            form.public_send("#{attribute}=", nil)
+          end
+        end
+      end
+    end
+  end
+end
