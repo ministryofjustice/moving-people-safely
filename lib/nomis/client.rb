a29967eedfe27e04e580f22e90f98cbad165c322
Dan Palmer <boss@bitrecursive.com>
2016-08-16 12:28:53 +0100

    Remove offender images retrieval for now
    Reintroduce this a bit further on

--- lib/nomis/client.rb
+++ lib/nomis/client.rb
@@ -2,18 +2,12 @@ require 'faraday'
 require 'json'
 require 'nomis/middlewares/parse_json'
 require 'nomis/models/details'
-require 'nomis/models/image'
 
 module Nomis
   class Client
     def offender_details(prison_number:)
       res = get(path: '/offender_details', prison_number: prison_number)
       res[:offenderdetails].map { |h| Details.new(h) }
-    end
-
-    def offender_image(prison_number:)
-      res = get(path: '/offender_images', prison_number: prison_number)
-      Image.new(res)
     end
 
     private
