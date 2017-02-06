69c3eeb45c4a3946e4d47e9d830d11d6ede2171e
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2017-02-06 12:05:10 +0000

    Re-design display of Must (not) return move info in profile page

--- app/presenters/move_presenter.rb
+++ app/presenters/move_presenter.rb
@@ -1,13 +1,26 @@
 class MovePresenter < SimpleDelegator
+  include ActionView::Helpers::OutputSafetyHelper
+
   def humanized_date
     date && date.to_s(:humanized)
   end
 
   def must_return_to
-    destinations.must_return_to.map(&:establishment).join(', ')
+    format_destinations(destinations.must_return_to)
   end
 
   def must_not_return_to
-    destinations.must_not_return_to.map(&:establishment).join(', ')
+    format_destinations(destinations.must_not_return_to)
+  end
+
+  private
+
+  def format_destinations(destinations)
+    return 'None' if destinations.empty?
+    safe_join(destinations.map do |destination|
+      array = [destination.establishment]
+      array << "(#{destination.reasons})" if destination.reasons.present?
+      array.join(' ')
+    end, '<br />'.html_safe)
   end
 end
