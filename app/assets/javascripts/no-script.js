7797f67874f2354303246938a89a8ffb0935fb3d
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2017-02-07 16:43:06 +0000

    Add test coverage for no-script javascript functionality

--- app/assets/javascripts/no-script.js
+++ app/assets/javascripts/no-script.js
@@ -1,10 +1,14 @@
 'use-strict';
 
-$(function () {
-  $('.no-script').each(function() {
-    $(this).removeClass('no-script');
-    $(this).find('input:disabled').each(function() {
-      $(this).prop('disabled', false);
+$(function() {
+  $.fn.removeNoScriptRestrictions = function() {
+    $('.no-script').each(function() {
+      $(this).removeClass('no-script');
+      $(this).find('input:disabled').each(function() {
+        $(this).prop('disabled', false);
+      });
     });
-  });
+  };
+
+  $.fn.removeNoScriptRestrictions();
 });
