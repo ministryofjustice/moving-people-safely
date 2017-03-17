1da12beed2e5ef18e088f8df8d7e38920034917f
Dan Palmer <boss@bitrecursive.com>
2017-03-17 12:03:25 +0000

    Override App Controller methods provided by Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,19 @@ class ApplicationController < ActionController::Base
       redirect_back(fallback_location: root_path)
     end
   end
+
+  def authenticate_user!
+    if session[:user_id].blank?
+      redirect_to new_session_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def user_signed_in?
+    session[:user_id].present?
+  end
+  helper_method :user_signed_in?
 end
