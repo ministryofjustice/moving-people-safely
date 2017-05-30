73bf42efc52201f2ce876795c1481958fa1db919
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2017-05-30 11:04:00 +0100

    Make offences data read only for issued PERs
    So far, after the PER had been issued it was still possible to attempt
    to change the offences information (although, once the form was
    submitted an error would be shown).
    
    These changes ensure that the user can no longer attempt to change that
    information, and that information is displayed in a table format rather
    than in a form as they were until now.
    
    Took the opportunity as well to do some small refactoring in the specs
    for the assessment summary pages and extract their common functionality
    into a mixin.

---
+++ spec/features/issued_per_spec.rb
@@ -0,0 +1,31 @@
+require 'feature_helper'
+
+RSpec.feature 'issued PERs', type: :feature do
+  scenario 'Checking issued PER sections are read only' do
+    login
+
+    escort = create(:escort, :issued)
+
+    visit escort_path(escort)
+
+    escort_page.confirm_read_only_detainee_details
+    escort_page.confirm_read_only_move_details
+
+    escort_page.confirm_healthcare_status('Complete')
+    escort_page.confirm_healthcare_action_link('View')
+    escort_page.click_edit_healthcare('View')
+    healthcare_summary.confirm_read_only
+    healthcare_summary.click_back_to_per_page
+
+    escort_page.confirm_risk_status('Complete')
+    escort_page.confirm_risk_action_link('View')
+    escort_page.click_edit_risk('View')
+    risk_summary.confirm_read_only
+    risk_summary.click_back_to_per_page
+
+    escort_page.confirm_offences_status('Complete')
+    escort_page.confirm_offences_action_link('View')
+    escort_page.click_edit_offences('View')
+    offences.confirm_read_only
+  end
+end
--- spec/features/pages/healthcare_summary.rb
+++ spec/features/pages/healthcare_summary.rb
@@ -1,9 +1,5 @@
 module Page
   class HealthcareSummary < Base
-    def confirm_status(expected_status)
-      within('header h3') do
-        expect(page).to have_content(expected_status)
-      end
-    end
+    include Page::AssessmentSummaryPageHelpers
   end
 end
