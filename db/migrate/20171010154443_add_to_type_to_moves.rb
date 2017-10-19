dd30c7ce42c849a658097d21a79ccc4d46bbe60f
Andy White <andy.white@digital.justice.gov.uk>
2017-10-19 16:13:12 +0100

    Applied review suggestions

--- db/migrate/20171010154443_add_to_type_to_moves.rb
+++ db/migrate/20171010154443_add_to_type_to_moves.rb
@@ -6,24 +6,24 @@ class AddToTypeToMoves < ActiveRecord::Migration[5.0]
       moves = Move.all
 
       moves.each do |move| 
-        unless move.to.nil?
-          to_type = case 
-                    when move.to.match(/Crown| CC|Luton County Court/i)
-                      'crown_court'
-                    when move.to.match(/Magistrates|Mag| MC| M C/)
-                      'magistrates_court'
-                    when move.to.match(/ST ALBANS/)
-                      'crown_court'
-                    when move.to.match(/HMP|the mount/i)
-                      'prison'
-                    when move.to.match(/treatment/i)
-                      'hospital'
-                    else
-                      'other'
-                    end
+        next if move.to.nil?
 
-          move.update_attribute(:to_type, to_type)
-        end
+        to_type = case 
+                  when move.to.match(/Crown| CC|Luton County Court/i)
+                    'crown_court'
+                  when move.to.match(/Magistrates|Mag| MC| M C/)
+                    'magistrates_court'
+                  when move.to.match(/ST ALBANS/)
+                    'crown_court'
+                  when move.to.match(/HMP|the mount/i)
+                    'prison'
+                  when move.to.match(/treatment/i)
+                    'hospital'
+                  else
+                    'other'
+                  end
+
+        move.update_attribute(:to_type, to_type)
       end
 
       moves.size
