8f1184236fae5a235d6789fab199f09ace816a02
Andy White <andy.white@digital.justice.gov.uk>
2017-10-19 15:22:58 +0100

    Make JS evaluation of radio concertina conditional

--- app/assets/javascripts/auto-complete.js
+++ app/assets/javascripts/auto-complete.js
@@ -1,14 +1,16 @@
-accessibleAutocomplete.enhanceSelectElement({
-    selectElement: document.querySelector('#crown_court'),
-    id: 'crown_court' // To match it to the existing <label>.
-});
+if($('#crown_court')[0]) {
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: document.querySelector('#crown_court'),
+        id: 'crown_court' // To match it to the existing <label>.
+    });
 
-accessibleAutocomplete.enhanceSelectElement({
-    selectElement: document.querySelector('#magistrates_court'),
-    id: 'magistrates_court' // To match it to the existing <label>.
-});
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: document.querySelector('#magistrates_court'),
+        id: 'magistrates_court' // To match it to the existing <label>.
+    });
 
-accessibleAutocomplete.enhanceSelectElement({
-    selectElement: document.querySelector('#prison'),
-    id: 'prison' // To match it to the existing <label>.
-});
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: document.querySelector('#prison'),
+        id: 'prison' // To match it to the existing <label>.
+    });
+}
