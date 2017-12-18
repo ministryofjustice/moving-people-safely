1c8f758664731a076ef1ef4ae7d66a682ebc3223
Andy White <andy.white@digital.justice.gov.uk>
2017-12-18 13:14:00 +0000

    Add SSO to healthcheck

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,18 @@
 require 'mojsso'
 
+module MovingPeopleSafely
+  class Application < Rails::Application
+    config.x.moj_sso_url = ENV.fetch('MOJSSO_URL', 'http://localhost:5000')
+  end
+end
+
 def initialize_omniauth
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider(
       :mojsso,
       ENV.fetch('MOJSSO_ID'),
       ENV.fetch('MOJSSO_SECRET'),
-      client_options: { site: ENV.fetch('MOJSSO_URL', 'http://localhost:5000') }
+      client_options: { site: Rails.application.config.x.moj_sso_url }
     )
   end
   OmniAuth.config.on_failure = SessionsController.action(:new)
