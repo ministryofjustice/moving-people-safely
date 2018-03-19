c77387aae9b23bea3595bf535a263a394ffeb575
Andy White <andy@wireworldmedia.co.uk>
2018-03-19 16:08:20 +0000

    Refactor using existing risk fetcher and mapper

---
+++ app/services/compare_auto_alerts/compare.rb
@@ -0,0 +1,79 @@
+module CompareAutoAlerts
+  class Compare
+    RISK_CACHE = 'risk_cache.json'.freeze
+    PAUSE_BETWEEN_API_CALLS = 5
+
+    def initialize(limit, pause = PAUSE_BETWEEN_API_CALLS)
+      @limit = limit
+      @pause = pause
+    end
+
+    def call
+      cached_risks = File.exist?(RISK_CACHE) ? JSON.parse(File.read(RISK_CACHE)) : {}
+
+      compare_escorts(cached_risks).tap do
+        File.open(RISK_CACHE, 'w') { |f| f.write(cached_risks.to_json) }
+      end
+    end
+
+    private
+
+    attr_reader :limit, :pause
+
+    def escorts
+      @_escorts ||= Escort.issued.limit(limit)
+    end
+
+    def compare_escorts(cached_risks)
+      escorts.each_with_object({}).with_index do |(escort, comparisons), i|
+        report(i, escort)
+        cached_risks[escort.prison_number] ||= fetch_mapped_risks(escort)
+        comparisons[escort.prison_number] = escort_details(
+          escort, cached_risks[escort.prison_number]
+        )
+        sleep pause
+      end
+    end
+
+    def report(i, escort)
+      @_tot ||= escorts.count
+      STDERR.puts "Comparing escort (#{i + 1}/#{@_tot}): #{escort.id} " \
+        "#{escort.prison_number}"
+    end
+
+    def fetch_mapped_risks(escort)
+      Detainees::RiskFetcher.new(
+        escort.prison_number, move_date: escort.issued_at.to_date
+      ).call.to_h
+    end
+
+    def escort_details(escort, mapped_risks)
+      {
+        id: escort.id,
+        issued_at: escort.issued_at,
+        to_type: escort.move.to_type,
+        age: format('%0.2f', ((Time.now - escort.issued_at) / 86_400.0)),
+        comparison: comparison(escort, mapped_risks)
+      }
+    end
+
+    def comparison(escort, mapped_risks)
+      mapped_risks.each_with_object({}) do |pair, comp|
+        attr = pair.first
+        val = pair.last
+        human = escort.risk.send(attr)
+        comp[attr.to_sym] = { human: human, auto: val, outcome: outcome(human, val) }
+      end
+    end
+
+    def outcome(human, auto)
+      return 'MATCH' if human == auto
+      truthy?(human) && !truthy?(auto) ? 'FALSE_NEGATIVE' : 'DIFFER'
+    end
+
+    def truthy?(expr)
+      return expr if [TrueClass, FalseClass].include?(expr.class)
+      /yes|true/i =~ expr
+    end
+  end
+end
--- app/services/compare_auto_alerts/csv_writer.rb
+++ app/services/compare_auto_alerts/csv_writer.rb
@@ -4,17 +4,12 @@ module CompareAutoAlerts
   class CsvWriter
     def self.write(comparisons)
       CSV.generate do |csv|
-        csv << %w[prison_number created_at to_type attribute human auto outcome]
+        csv << %w[escort_id prison_number issued_at to_type attribute human auto outcome days_old]
 
         comparisons.each do |prison_number, data|
           data[:comparison].each do |attr, details|
-            csv << [prison_number,
-                    data[:created_at],
-                    data[:to_type],
-                    attr,
-                    details[:human],
-                    details[:auto],
-                    details[:outcome]]
+            csv << [data[:id], prison_number, data[:issued_at], data[:to_type],
+                    attr, details[:human], details[:auto], details[:outcome], data[:age]]
           end
         end
       end
