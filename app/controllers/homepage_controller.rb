f7af2998ab76ea068d7e59a9ff8e9f39be500595
Andy White <andy@wireworldmedia.co.uk>
2018-05-15 16:10:01 +0100

    Separate search bar

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,40 +1,21 @@
 class HomepageController < ApplicationController
   before_action :redirect_to_court_view, if: :court_user?
-  before_action :validate_search, only: :show
-  before_action :authorize_user_to_access_prisoner!, only: :show, if: :valid_search?
 
   def show
     @escorts = Escort.for_date(date_picker.date).for_user(current_user)
     render :show, locals: locals
   end
 
-  def detainees
-    redirect_to root_path(search_params)
-  end
-
   def escorts
     date_picker.date = params[:escorts_due_on]
     session[:escorts_due_on] = date_picker.to_s
-    redirect_to root_path(search_params)
+    redirect_to root_path
   end
 
   private
 
-  def validate_search
-    search_form.validate(search_params) if search_params.present?
-  end
-
-  def valid_search?
-    search_params.present? && search_form.valid?
-  end
-
-  def prison_number
-    search_form.prison_number
-  end
-
   def locals
     {
-      search_form: search_form,
       date_picker: date_picker,
       prison_number: params[:prison_number],
       dashboard: DashboardPresenter.new(@escorts)
@@ -43,13 +24,5 @@ class HomepageController < ApplicationController
 
   def date_picker
     @_date_picker ||= DatePicker.new(session[:escorts_due_on])
-  end
-
-  def search_form
-    @_search_form ||= Forms::Search.new
-  end
-
-  def search_params
-    params.permit(:prison_number).delete_if { |_key, value| value.blank? }
   end
 end
