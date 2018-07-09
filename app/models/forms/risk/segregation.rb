532746e31d1b3471478d315b7b2a40e78c6762bb
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2018-07-09 17:54:52 +0100

    Align prison and police alerts

--- app/models/forms/risk/segregation.rb
+++ app/models/forms/risk/segregation.rb
@@ -5,8 +5,12 @@ module Forms
       POLICE_CSRA_CHOICES = %w[yes no].freeze
 
       options_field :csra, options: :csra_choices
+      property :csra_details, type: StrictString
       options_field :rule_45, if: :from_prison?
       options_field_with_details :vulnerable_prisoner
+
+      validates :csra_details, presence: true, if: -> { csra == 'yes' && from_police? }
+      reset attributes: %i[csra_details], if_falsey: :csra
 
       def csra_choices
         return PRISON_CSRA_CHOICES if from_prison?
