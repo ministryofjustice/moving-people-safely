d8b10c4f2695e273eb9343266042295b509bfd5a
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2018-08-28 11:44:39 +0100

    Update to Circle CI 2.0

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -2,4 +2,4 @@ require 'rails_helper'
 require 'site_prism'
 
 Dir[Rails.root.join("spec/features/support/*.rb")].each { |f| require f }
-Dir[Rails.root.join("spec/features/pages/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/features/pages/*.rb")].sort.each { |f| require f }
