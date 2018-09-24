967219cf9163882018af0f21c1e63921ae1c5ea5
Andy White <andy@wireworldmedia.co.uk>
2018-09-24 10:07:36 +0100

    Add missing detainee.security_category to schema.rb
    Due to an earlier kerfuffle, this change was omitted.

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_10_03_085856) do
     t.string "peep"
     t.text "peep_details"
     t.text "interpreter_required_details"
+    t.string "security_category"
     t.index ["escort_id"], name: "index_detainees_on_escort_id"
     t.index ["prison_number"], name: "index_detainees_on_prison_number"
   end
