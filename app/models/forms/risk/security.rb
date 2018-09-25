dac1d8b2e8ac887f9bc01cb70ca669c824d4ee7e
Andy White <andy@wireworldmedia.co.uk>
2018-09-25 15:19:22 +0100

    Remove category_a references
    These crept in from work Cesi did on his huge 'Change police risk questions' PR which is now merged. Also changes required to layout of print views.

--- app/models/forms/risk/security.rb
+++ app/models/forms/risk/security.rb
@@ -2,7 +2,6 @@ module Forms
   module Risk
     class Security < Forms::Base
       options_field_with_details :controlled_unlock, if: :from_prison?
-      options_field :category_a, if: :from_prison?
       options_field_with_details :high_profile
       options_field_with_details :violent_or_dangerous, if: :from_police?
       options_field_with_details :previous_escape_attempts, if: :from_police?
