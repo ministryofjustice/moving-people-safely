614f3513cdde2c1f54e1fb86d890da5ae3067f49
Andy White <andy@wireworldmedia.co.uk>
2019-01-08 10:12:43 +0000

    Require details on all observation levels
    Ticket: https://trello.com/c/zN3vJVuW

--- app/helpers/risks_helper.rb
+++ app/helpers/risks_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module RisksHelper
-  def heightened_observation_yes_no(level)
-    if Forms::Risk::RiskToSelf::OBSERVATION_LEVELS_REQUIRING_DETAILS.include?(level)
-      'yes'
-    else
-      'no'
-    end
-  end
-
   def full_observation_level_details(level, details)
     return '' if level == Forms::Risk::RiskToSelf::OBSERVATION_LEVELS.first
 
